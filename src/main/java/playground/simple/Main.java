eb4a65bf2bdbb13ec279ca1ebef45c73166e88fe
czerny <czerny.jakug@gmail.com>
2015-12-17 11:24:29 +0100

    Stream limit and skip of short streams

--- src/main/java/playground/simple/Main.java
+++ src/main/java/playground/simple/Main.java
@@ -7,6 +7,7 @@ import java.util.stream.Stream;
 public class Main {
     public static void main(String[] args) {
         final List<Integer> list = Stream.of(1, 2)
+                .skip(10)
                 .limit(10)
                 .collect(Collectors.toList());
         System.out.println(list);
