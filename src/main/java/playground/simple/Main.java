bfc1060aca3b8961140ffb18b52461199ad9ff75
czerny <czerny.jakug@gmail.com>
2015-12-17 11:22:08 +0100

    Stream.limit() of short stream

---
+++ src/main/java/playground/simple/Main.java
@@ -0,0 +1,14 @@
+package playground.simple;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Main {
+    public static void main(String[] args) {
+        final List<Integer> list = Stream.of(1, 2)
+                .limit(10)
+                .collect(Collectors.toList());
+        System.out.println(list);
+    }
+}
