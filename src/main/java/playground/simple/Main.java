12c21a59e8ecb2e042cfdf2f0a1e86ca561126cb
czerny <czerny.jakug@gmail.com>
2016-01-08 18:01:48 +0100

    Stream of 'null'

--- src/main/java/playground/simple/Main.java
+++ src/main/java/playground/simple/Main.java
@@ -1,11 +1,10 @@
 package playground.simple;
 
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args) {
-        final double x = 81;
-        final double n = 4;
-        final double nthRoot = Math.pow(Math.E, Math.log(x) / n);
-        System.out.println(nthRoot);
+        System.out.println(Stream.of(null));
     }
 
 }
