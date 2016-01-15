bf8be7ea1dfbad79716cdbbf7b1bf9f178157b97
czerny <czerny.jakug@gmail.com>
2016-01-15 18:03:39 +0100

    streams: exception throw in mapper, forEach printed

--- src/main/java/playground/simple/Main.java
+++ src/main/java/playground/simple/Main.java
@@ -1,14 +1,12 @@
 package playground.simple;
 
-import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        final int[] ints = IntStream.rangeClosed(5, 12)
+        IntStream.rangeClosed(5, 12)
                 .map(Main::inc)
-                .toArray();
-        System.out.println(Arrays.toString(ints));
+                .forEach(System.out::println);
     }
 
     private static int inc(int i) {
