a71454043faffff628c4fceee46b2857cad99b86
czerny <czerny.jakug@gmail.com>
2016-01-15 18:00:28 +0100

    streams: exception throw in mapper, collected

--- src/main/java/playground/simple/Main.java
+++ src/main/java/playground/simple/Main.java
@@ -1,10 +1,20 @@
 package playground.simple;
 
-import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Stream.of(null));
+        final int[] ints = IntStream.rangeClosed(5, 12)
+                .map(Main::inc)
+                .toArray();
+        System.out.println(Arrays.toString(ints));
     }
 
+    private static int inc(int i) {
+        if (i > 10) {
+            throw new RuntimeException("greater than 10");
+        }
+        return i + 1;
+    }
 }
