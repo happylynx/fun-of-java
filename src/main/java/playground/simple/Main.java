1a6bd8a1dc1f86e58713c1042ce6111854578324
czerny <czerny.jakug@gmail.com>
2016-07-20 01:39:53 +0200

    empty matching groups

--- src/main/java/playground/simple/Main.java
+++ src/main/java/playground/simple/Main.java
@@ -1,9 +1,14 @@
 package playground.simple;
 
-import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Arrays.toString("abc".split("")));
+        final Matcher matcher = Pattern.compile("^(\\w{2})(_(\\w{2}))?$").matcher("en");
+        matcher.find();
+        IntStream.rangeClosed(1, 3)
+                .forEach(index -> System.out.println(index + " " + matcher.group(index)));
     }
 }
