c27284c1a303cb14dc750215f4854b35884e5fa2
happylynx <czerny.jakug@gmail.com>
2016-11-06 04:14:56 +0100

    array initializer in Collection#toArray()

--- src/main/java/playground/simple/Main.java
+++ src/main/java/playground/simple/Main.java
@@ -1,15 +1,11 @@
 package playground.simple;
 
-import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
-        final ArrayList<StackTraceElement> stackFrame = new ArrayList<>();
-        for (int i : new int[] {1, 2}) {
-            final StackTraceElement frame = Thread.currentThread().getStackTrace()[0];
-            stackFrame.add(frame);
-        }
-        final boolean equals = stackFrame.get(0).equals(stackFrame.get(1));
-        System.out.println(equals);
+        Collections.singletonList(new A()).toArray({});
     }
+
+    private static class A {}
 }
