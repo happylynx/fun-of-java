72ee0db97b575c27e9aa1e1731aecebf8ce96e8e
happylynx <czerny.jakug@gmail.com>
2017-02-18 11:06:56 +0100

    returned null can be casted to any object - but there are compilation limits

--- src/main/java/playground/simple/Main.java
+++ src/main/java/playground/simple/Main.java
@@ -2,14 +2,16 @@ package playground.simple;
 
 public class Main {
     public static void main(String[] args) {
-        A a = (A) getNull();
+        A a = (A) getNull(); // fails during compilation
         System.out.println("ok");
     }
 
-    public static Object getNull() {
+    public static B getNull() {
         return null;
     }
 
 }
 
 class A {}
+
+class B {}
